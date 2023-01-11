0f0568ae739f96550a3b9e329ae3ba6077cb7b68
Michał Stępień <michal.stepien@ringieraxelspringer.pl>
2023-01-11 21:29:56 +0100

    initial commit

---
+++ l1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "Kazek";
+console.log(username);
+let a = 12;
+let b = 5;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ l1/src/main.ts
@@ -0,0 +1,10 @@
+let username = "Kazek";
+console.log(username);
+
+let a: number = 12;
+let b: number = 5;
+let c: number = 2;
+
+console.log(a / b)
+
+console.log(c * b)
